16fde6e60096c4793b17aac092a48ffe6ff1e3fe
Kenneth Lin <kenlin@berkeley.edu>
2014-05-03 06:06:50 -0700

    using redis to go

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ var fs = require('fs');
 var redis = require('redis');
 var express = require('express');
 
-var client = redis.createClient();
-
 var app = express();
 var router = express.Router();
+
+if (process.env.REDISTOGO_URL) {
+  var rtg = require('url').parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(':')[1]);
+} else {
+  var client = redis.createClient();
+}
 
 router.param('id', function(req, res, next, id) {
   next();
 });
 
 router.get('/:id', function(req, res) {
-  client.get(req.params.id, function(err, reply) {
+  redis.get(req.params.id, function(err, reply) {
     if (!err) {
       res.send(reply);
     }
